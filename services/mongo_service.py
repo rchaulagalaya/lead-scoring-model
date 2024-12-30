d7e65098250e9a5ad570adab68094ab4a0b42281
rchaulagalaya <rupa.c@homeloanexperts.com.au>
2024-12-30 11:00:03 +0545

    Initial Commit

---
+++ services/mongo_service.py
@@ -0,0 +1,40 @@
+import sys, os
+import logging
+from pymongo import MongoClient
+# set logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class dbConnection:
+	def get_mongo_connection(self):
+		logger.info("Getting Mongo DB Connection.")
+
+		username ="mongouser"
+		password="H8zYgzuQbTb5psyc"
+
+		try:
+			# mongo_server = "mongodb+srv://{username}:{password}@cluster0-dk5qf.mongodb.net/"
+			# mongo_server = "mongodb://localhost:27017/"
+			mongo_server = "mongodb+srv://mongouser:mPtojUhz4TadduZ6@cluster0-8zgt8.mongodb.net/test?retryWrites=true&w=majority"
+
+			logger.info("mongo_server : ")
+			logger.info(mongo_server)
+
+			connection = MongoClient(mongo_server.format(username=username, password=password))
+
+			# logger.info("mongo connection  : ")
+			logger.info("Received Mongo DB Connection.")
+
+			return connection
+		except Exception as ex:
+			logger.error(f"Mongo Database connection failed. Error {ex}")
+
+	def close_mongo_connection(self, connection):
+		try:
+			connection.close()
+			logger.info("Closed Mongo DB Connection.")
+
+		except Exception as ex:
+			logger.error("Mongo Database connection failed to Close. Error %s", ex)
+
